3cc940e11a8c60f5b32ad6553955a85180e5ab5f
fffffernandez <145748076+fffffernandez@users.noreply.github.com>
2023-09-22 11:09:55 +0200

    Add files via upload

---
+++ dia one.py
@@ -0,0 +1,29 @@
+Hola Mundo
+Me llamo Ruben
+Ruben
+5
+5+8
+13
+print(5)    #imprime 5.
+5
+print (x)
+5
+#Creamos variable con texto -> string
+nombre = "Ruben"
+print (nombre)
+Ruben
+print("sumo nombre+numero:")
+sumo nombre+numero:
+print ("Ruben"+5)
+print('print (nombre+str(numero)):')
+print (nombre+str(numero)):
+print (nombre+str(numero))
+print("CuCu*10")
+"\n\\Hacemos \"operaciones\"\t matematic
+num1 = 2
+num2 = 8
+suma = num1+num2
+resta= num1-num2
+print("suma= " + str(suma) + " | Resta = " str(resta))
+| multiplicacion=" str(multp) +"\
+                  
