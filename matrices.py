f759cf0bddc0781c4c921c0179c2645af43745c9
rubenffernandez <145748076+fffffernandez@users.noreply.github.com>
2023-11-27 14:24:11 +0100

    Add files via upload

---
+++ matrices.py
@@ -0,0 +1,24 @@
+#-> Sumar todos los elementos de la matriz
+#            matriz= [[1,2,3],[4,5,6],[7,8,9]]
+#-> Sumar solo los elementos de la fila indicada.
+#-> Sumar solo los elementos de la columna indicada.
+#-> Sumar solo los elementos que están en posiciones pares.[0,0]
+#-> Sumar solo los elementos que están en posiciones impares.
+#-> Dada otra matriz, sumar los elementos que hay en la posición.
+"""lista1 = [1,2,3]
+lista2 = [4,5,6]
+lista3 = [7,8,9]
+Matriz = [lista1,lista2,lista3]
+
+numeroFilas = 3
+numeroColumnas = 3
+matriz=[]
+for numero in range():"""
+matriz=[1,2,3],[4,5,6],[7,8,9]
+def sumarTodosElementos(matriz):
+    for numeroFila in range(len(matriz)):
+        for numeroColumna in range(len(matriz[numeroFila])):
+            suma+=matriz[numeroFila][numeroColumna]
+    return suma 
+print("sumar todos los elementos pares de la matriz: ")
+print(sumarTodosElementos(matriz))
